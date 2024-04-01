c6864b46d626da47c42eaf15df79ad6078877930
upadhyayaditi <au.aditi08@gmail.com>
2024-04-01 23:29:25 +0530

    register page updated

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,47 +1,49 @@
-import React, { useState } from 'react'
-import "./Login.scss"
-import newRequest from '../../utils/newRequest'
-import { useNavigate } from "react-router-dom"
+import React, { useState } from "react";
+import "./Login.scss";
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState(null)
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
-      const res = await newRequest.post("/auth/login", {username, password});
-      localStorage.setItem("currentUser", JSON.stringify(res.data))
+      const res = await newRequest.post("/auth/login", { username, password });
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
       navigate("/")
     } catch (err) {
-      setError(err.response.data)
-      
+      setError(err.response.data);
     }
-    
-  }
+  };
 
   return (
-    <div className='login'>
+    <div className="login">
       <form onSubmit={handleSubmit}>
         <h1>Sign in</h1>
         <label htmlFor="">Username</label>
-        <input type="text" name='username' placeholder='johndeo' 
-        onChange={e => setUsername(e.target.value)}
+        <input
+          name="username"
+          type="text"
+          placeholder="johndoe"
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <label htmlFor="">Password</label>
-        <input type="password" name="password"
-         onChange={e => setPassword(e.target.value)}
+        <input
+          name="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button type='submit' >Login</button>
+        <button type="submit">Login</button>
         {error && error}
       </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "http://localhost:8800/api/", 
-    withCredentials: true,
-})
+  baseURL: "http://localhost:8800/api/",
+  withCredentials: true,
+});
 
-export default newRequest
+export default newRequest;
